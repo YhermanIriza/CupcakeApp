eba7d80a5ea8dcdbe4bc0f4f672bf86e28c3b4f0
YhermanIriza <yhermanyj@gmail.com>
2025-09-27 14:33:13 -0500

    implementar TopAppBar

--- app/src/main/java/com/example/cupcakeapp/CupcakeScreen.kt
+++ app/src/main/java/com/example/cupcakeapp/CupcakeScreen.kt
@@ -8,10 +8,34 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.cupcakeapp.ui.theme.CupcakeAppTheme
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun CupcakeAppBar(
+    modifier: Modifier = Modifier
+) {
+    TopAppBar(
+        title = {
+            Text(text = "Cupcake App")
+        },
+        colors = TopAppBarDefaults.mediumTopAppBarColors(
+            containerColor = MaterialTheme.colorScheme.primaryContainer
+        ),
+        modifier = modifier
+    )
+}
 
 @Composable
 fun CupcakeApp() {
-    Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+        topBar = {
+            CupcakeAppBar()
+        }
+    ) { innerPadding ->
         Greeting(
             name = "Android",
             modifier = Modifier.padding(innerPadding)
@@ -19,10 +43,10 @@ fun CupcakeApp() {
     }
 }
 
+@Preview(showBackground = true)
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun CupcakeAppPreview() {
+    CupcakeAppTheme {
+        CupcakeApp()
+    }
 }
